51aa54638c07d4bf16a66d20ac09a1a1cdf44889
iamfromkirov <iamfromkirov@gmail.com>
2025-12-12 15:28:38 +0300

    Add files via upload

---
+++ radiobot.py
@@ -0,0 +1,24 @@
+# bot.py
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+BOT_TOKEN = "7978336311:AAEbdxHSd24abiMUtP7A7vMuhN-sXGVluRw"
+WEB_APP_URL = "https://iamfromkirov.github.io/radio3038/radio.html"  # ← ваша Web App
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    web_app = types.WebAppInfo(url=WEB_APP_URL)
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=[
+        [types.InlineKeyboardButton(text="▶️ Слушать радио", web_app=web_app)]
+    ])
+    await message.answer("Нажмите кнопку, чтобы включить радио:", reply_markup=keyboard)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
